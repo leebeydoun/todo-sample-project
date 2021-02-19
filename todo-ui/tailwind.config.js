77dc4b19154ae46e7a42af561485df021880d25e
Lee <eldestory@gmail.com>
2021-02-19 04:05:57 +0000

    Updated gitignore and added tailwindcss configuration

---
+++ todo-ui/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.tsx',
+    './src/**/*.ts',
+    './public/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
